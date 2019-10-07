ba470c2e6300d774e8ab981c72316b5082099b8c
milimolo <bondemand@hotmail.dk>
2019-10-07 10:18:09 +0200

    customer entity

---
+++ WebShop.Core.Entity/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebShop.Core.Entity
+{
+    class Customer
+    {
+        public int id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string address { get; set; }
+        public int zipAddress { get; set; }
+        public int phoneNumber { get; set; }
+        public int age { get; set; }
+        public List<Shoe> cartList { get; set; }
+    }
+}
--- WebShop.Core.Entity/Shoe.cs
+++ WebShop.Core.Entity/Shoe.cs
@@ -7,5 +7,12 @@ namespace WebShop.Core.Entity
     public class Shoe
     {
         public int id { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        public int size { get; set; }
+        public string brand { get; set; }
+        public string color { get; set; }
+        public string type { get; set; }
+        public DateTime date { get; set; }
     }
 }
