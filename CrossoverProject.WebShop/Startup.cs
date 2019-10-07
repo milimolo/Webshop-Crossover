1f4425f0913da71f892052a3eed3a942ade7e5b9
milimolo <bondemand@hotmail.dk>
2019-10-07 14:11:10 +0200

    Update Startup.cs

--- CrossoverProject.WebShop/Startup.cs
+++ CrossoverProject.WebShop/Startup.cs
@@ -6,10 +6,17 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using Webshop.Infrastructure.Data;
+using Webshop.Infrastructure.Data.Repositories;
+using WebShop.Core.ApplicationService;
+using WebShop.Core.ApplicationService.Impl;
+using WebShop.Core.DomainService;
 
 namespace CrossoverProject.WebShop
 {
@@ -25,6 +32,20 @@ namespace CrossoverProject.WebShop
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<IShoeRepository, Webshop.Infrastructure.Data.Repositories.ShoeRepository>();
+            //Laver en database ved navn ShoeApp.db
+            services.AddDbContext<WebshopAppContext>(
+                opt => opt.UseSqlite("Data Source=ShoeApp.db"));
+
+            services.AddScoped<IShoeRepository, ShoeRepository>();
+            services.AddScoped<IShoeService, ShoeService>();
+
+            services.AddMvc().AddJsonOptions(options =>
+            {
+                options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+                //options.SerializerSettings.MaxDepth = 2;
+            });
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
@@ -33,6 +54,14 @@ namespace CrossoverProject.WebShop
         {
             if (env.IsDevelopment())
             {
+                using (var scope = app.ApplicationServices.CreateScope())
+                {
+                    var context = scope.ServiceProvider.GetRequiredService<WebshopAppContext>();
+                    context.Database.EnsureDeleted();
+                    context.Database.EnsureCreated();
+                    DBInitializer.Seed(context);
+                }
+
                 app.UseDeveloperExceptionPage();
             }
             else
