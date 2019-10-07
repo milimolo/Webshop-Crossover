bfe4155064fbc49c33c9b6d8d9e3bbe9724acd5d
milimolo <bondemand@hotmail.dk>
2019-10-07 13:49:22 +0200

    dbInitializer

--- Webshop.Infrastructure.Data/DBInitializer.cs
+++ Webshop.Infrastructure.Data/DBInitializer.cs
@@ -1,10 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using WebShop.Core.Entity;
 
 namespace Webshop.Infrastructure.Data
 {
     public class DBInitializer
     {
+        public static void Seed(WebshopAppContext wac)
+        {
+            wac.Database.EnsureDeleted();
+            wac.Database.EnsureCreated();
+
+            Shoe shoe1 = new Shoe()
+            {
+                name = "Hot Heels",
+                description = "Perfect shoe for running",
+                color = "Black",
+                brand = "Nike",
+                size = 40,
+                type = "Herre",
+                date = DateTime.Today.AddDays(-5)
+            };
+
+            Shoe shoe2 = new Shoe()
+            {
+                name = "Off the Court",
+                description = "Leave the competition in the dust",
+                color = "White",
+                brand = "Adidas",
+                size = 39,
+                type = "Herre",
+                date = DateTime.Today.AddDays(-10)
+            };
+
+            Shoe shoe3 = new Shoe()
+            {
+                name = "Nice Walkers",
+                description = "Shoe great for a long walk",
+                color = "Red",
+                brand = "Puma",
+                size = 35,
+                type = "Kvinde",
+                date = DateTime.Today.AddDays(-1)
+            };
+
+            shoe1 = wac.Shoes.Add(shoe1).Entity;
+            shoe2 = wac.Shoes.Add(shoe2).Entity;
+            shoe3 = wac.Shoes.Add(shoe3).Entity;
+
+            wac.SaveChanges();
+
+        }
     }
 }
